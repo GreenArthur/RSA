5decda8ab8ede33d0f89b36e45a885d3a65c7914
GreenArthur <90436099+GreenArthur@users.noreply.github.com>
2021-09-24 00:57:30 -0700

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -99,7 +99,7 @@ def GenerateKeys(p,q):
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
-    #find the inverse of e and phi
+    #satisfy the congruence relation 
     d = modInverse(e,phi)
 
     return ((e, mod), (d, mod))
